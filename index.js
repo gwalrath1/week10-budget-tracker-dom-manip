1ebb91ee25295fec0a4deb221bb9195bdd9ff445
gabriella walrath <gabriella.walrath@gmail.com>
2026-06-02 20:19:49 -0400

    added radio buttons for purchase category; edited create new row function to add a cell with a trashcan icon; created array to store transactions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 /* ADD BUDGET */
 
+let purchasesTable = document.querySelector("#purchases-table > tbody");
 const budgetButton = document.querySelector('#budget-button');
 const budgetTable = document.querySelector('#budget-table');
 
@@ -14,14 +15,31 @@ budgetButton.addEventListener('click', (e) => {
 /* ADD PURCHASE */
 
 const addPurchaseButton = document.querySelector("#add-purchases-button");
-const purchasesTable = document.querySelector("#purchases-table > tbody");
 
 addPurchaseButton.addEventListener('click', (e) => {
     // 1. Get the CURRENT values from the inputs inside the click event
     let purchase = document.querySelector("#purchase").value;
     let amount = document.querySelector("#amount").value;
     let date = document.querySelector("#date").value;
-    let category = document.querySelector("#category").value;
+
+    
+    let categoryRadioBtns = document.querySelectorAll('input[name="categories"]');
+    
+
+    let selectedCategory;
+    for (let categoryRadioBtn of categoryRadioBtns) {
+      if(categoryRadioBtn.checked) {
+        selectedCategory = categoryRadioBtn.value;
+        break;
+      }
+    }
+
+    console.log(`you selected ${selectedCategory}`);
+    console.log(amount);
+    console.log(date);
+    console.log(purchase);
+
+    
 
 // ------------------------------------------------- // 
 
@@ -37,48 +55,52 @@ addPurchaseButton.addEventListener('click', (e) => {
      return template.content.firstElementChild;
 
    };
-    let budgetLeft = document.querySelector("#budget-entered").value;
-    let balance = 0;
+    let totalBudget = document.querySelector("#budget-entered").value;
+    
+    
+    
+
+
     let rowPurchases = createHTMLElement(
         `<tr>
             <td>${purchase}</td>
             <td>$${amount}</td>
             <td>${date}</td>
-            <td>${category}</td>
-            <td>${budgetLeft - amount}</td>
+            <td>${selectedCategory}</td>
+            <td>${totalBudget - amount}</td>
+            <td>
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                      <path
+                        d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
+                  </svg>
+                </button>
+            </td>
         </tr>`
     );
 
 /* PURCHASE ADDED TO DOM */
     purchasesTable.append(rowPurchases);
 
-/* EXPENSES CALCULATED, PUT INTO SUMMARY TABLE */
+/* CALCULATE EXPENSES */
 
-    let totalExpenses = document.querySelector("#total-expenses");
-    let balanceDyanmic = document.querySelector("#balance-in-table").value;
-    totalExpenses = balanceDyanmic - amount;
+    let transactionsArray = [];
+
+    for (let i = 0; i < transactionsArray.length; i++) {
+      transactionsArray.push(amount);
+      let expensesTotal = 0;
+      expensesTotal = expensesTotal + transactionsArray[i];
+      console.log("Your total number of expenses is " + expensesTotal);
+    }
+
+    
 });
 
- 
-    const totalExpensesSummary = document.querySelector("#total-expenses-summary");
-    
-    // to calculate the total expenses I have to use for of and make an array to store the sum of expenses to subtract from the entered budget.
-
-    let listOfExpenses = [];
-
-    function sumOfExpenses(amount) {
-        listOfExpenses.push(amount.value);
-    console.log("These are all of the expenses: " + listOfExpenses);
-    }
-    
-   sumOfExpenses();
-
-/* BALANCE IS CALCULATED AND ADDED TO BALANCE COLUMN AND BUDGET LEFT COLUMN */
 
 
 /* PIE CHART */
 
-var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+var xValues = ["Home/Utilities", "Transportation", "Groceries", "Insurance", "Restaurants, Shopping, Entertainment"];
 var yValues = [55, 49, 44, 24, 15];
 var barColors = [
   "#b91d47",
@@ -100,7 +122,7 @@ new Chart("myChart", {
   options: {
     title: {
       display: true,
-      text: "World Wide Wine Production 2018"
+      text: ""
     }
   }
 });
